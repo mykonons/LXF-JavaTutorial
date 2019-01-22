07eeded2d21e7aa922b6b38ede2a1bce5574501a
Han Zhang <stephenzhang0713@outlook.com>
2019-01-23 01:35:47 +0800

    廖雪峰Java视频代码

---
+++ Java异常处理/src/CommonsLogging/Main.java
@@ -0,0 +1,19 @@
+package CommonsLogging;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Main {
+
+  static Log log= LogFactory.getLog(Main.class);
+
+  public static void main(String[] args) {
+    Person p=new Person("Xiao Ming");
+    log.info(p.hello());
+    try {
+      new Person(null);
+    } catch (Exception e){
+      log.error("Exception",e);
+    }
+  }
+}
---
+++ Java异常处理/src/CommonsLogging/Person.java
@@ -0,0 +1,47 @@
+package CommonsLogging;
+
+public class Person {
+  private String name;
+  private int age;
+  private boolean gender;
+  private String address;
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public boolean isGender() {
+    return gender;
+  }
+
+  public void setGender(boolean gender) {
+    this.gender = gender;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  public String hello(){
+    return "Hello,"+this.name;
+  }
+}
